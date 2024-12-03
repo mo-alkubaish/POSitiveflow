9982b3f02231644fac9a7bd163eb6cb5eb9ec383
mo.alkubaish <mohammed.a.j.alkubaish@gmail.com>
2024-12-03 14:51:52 +0300

    deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   typescript: {
     ignoreBuildErrors: true,
   },
