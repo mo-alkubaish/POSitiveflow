eb11ad96b39d1485ebc532c932e4ba32d8ab60fc
mo-alkubaish <53779698+mo-alkubaish@users.noreply.github.com>
2024-12-08 16:23:38 +0300

    Update update-admin.dto.ts

--- backend/src/admin/dto/update-admin.dto.ts
+++ backend/src/admin/dto/update-admin.dto.ts
@@ -1,4 +1,2 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAdminDto } from './create-admin.dto';
 
-export class UpdateAdminDto extends PartialType(CreateAdminDto) {}
+export class UpdateAdminDto {}
